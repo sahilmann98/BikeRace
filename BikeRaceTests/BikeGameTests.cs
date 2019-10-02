86370a9bbc4c19148177341474afde99e88eafcc
sahilmann98 <54046468+sahilmann98@users.noreply.github.com>
2019-10-02 23:52:16 +1300

    Add files via upload

---
+++ BikeRaceTests/BikeGameTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BikeRace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRace.Tests
+{
+    [TestClass()]
+    public class BikeGameTests
+    {
+        [TestMethod()]
+        public void moveBikeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
